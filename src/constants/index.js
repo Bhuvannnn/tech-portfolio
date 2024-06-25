ee4c96fac98d6e706f5476276d6e6adefcbb0fd7
Bhuvan Shah <bhuvanshah288@gmail.com>
2024-06-24 22:23:36 -0700

    Changed text in index file

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,70 +3,76 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `As a passionate graduate student at the University of Southern California, I specialize in crafting advanced Natural Language Processing (NLP) and Generative AI solutions. With 1.5 years of hands-on experience, I've developed expertise in building Large Language Models (LLMs) like Retrieval-Augmented Generation (RAG), Named Entity Recognition (NER) with DeBERTaV3, and BiLSTM-CNN architectures. Alongside my proficiency in LLMs, I also possess robust skills in full-stack development with technologies such as React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to blend my deep technical knowledge in AI with my web development skills to create innovative, scalable applications that deliver exceptional user experiences and drive business growth.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile professional specializing in software solutions majorly including Natural Language Processing (NLP) and Generative AI. Currently, as a graduate student at the University of Southern California, I am honing my skills in building advanced NLP architectures, including custom LSTM networks, RAG models, and fine-tuned transformers like DeBERTaV3. With a solid foundation in full-stack development, I have 1.5 years of experience working with front-end technologies like React and Next.js, and back-end technologies including Node.js, MySQL, PostgreSQL, and MongoDB. My journey in technology is driven by a deep curiosity and a commitment to solving complex problems through innovative solutions. I thrive in collaborative environments where I can contribute to cutting-edge projects and continuously learn new skills. Outside of my technical pursuits, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
     year: "2024 - Present",
-    role: "Teaching Assistant",
+    role: "Teaching Associate",
     company: "University of Southern California",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    description: `As a Teaching Associate for CSCI 526 - Advanced Mobile Devices and Game Consoles a masters level course at the University of Southern California, I support the academic journey of 90 students. My responsibilities include conducting office hours where I provide guidance on C# coding challenges and course concepts. 
+    I also create and deliver engaging lectures that cover key aspects of mobile and game console development. 
+    Additionally, I grade assignments and projects, offering detailed feedback to help students refine their work across various project phases. 
+    I develop innovative in-class coding activities to reinforce their learning and assist in integrating analytics into their games.`,
+    technologies: ["C#", "Python", "SQL", "Firebase"],
   },
   {
-    year: "2022 - 2023",
+    year: "December 2022 - May 2023",
     role: "Data Scientist Intern", 
     company: "Prompt Softech",
     description: `Created a custom BiLSTM - CNN neural network with a bifurcated architecture, achieving an impressive 83% accuracy.
     Built and fine-tuned proprietary technical indicators, achieving an average accuracy of 83%.
     Conducted comprehensive back testing, resulted in a substantial accuracy boost to 88%, while refining indicator parameters for optimal performance through MetaTrader using MQL4 and MQL5 scripting languages`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    technologies: ["PyTorch", ,"ML", "SQL", "Tensorflow", "MetaTrader", "MQL", "JavaScript", "React.js", "Node.js"],
   },
   {
-    year: "2021 - 2022",
+    year: "June 2021 - July 2021",
     role: "Data Analyst Intern",
     company: "Tornado Computers",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    description: `Collaborated on a project leveraging algorithms and IOT to optimize hardware performance; identifying bottlenecks, fine-tuning
+                  system parameters, and predicting system failures, attaining a remarkable 92% accuracy in performance optimization
+                  Implemented neural network for predictive maintenance by collecting and analyzing data from hardware sensors and logs, resulting in an 87% accuracy rate, reducing downtime and repair costs.`,
+    technologies: ["Python", "IOT", "Raspberry Pi", "Postgres", 'Spatial Recognition'],
   },
   {
-    year: "2020 - 2021",
+    year: "June 2020 - October 2020",
     role: "Software Developer Intern",
     company: "VueInternational",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    description: `Took proactive measures in designing and implementing interactive components using HTML, CSS, and JavaScript, seamlessly integrating with a sqlite3 database to facilitate secure credit card transactions through the Stripe API
+Integrated dynamic components into company's website, sparking a substantial 15% surge in user engagement and modeled a commendable 20% enhancement in transaction security.`,
+    technologies: ["SqLite", "Stripe", "API Integration"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Assessing and Masking of Personally Identifiable Information (PII)",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+      ".",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "Me, Myself and Time: A Game based on Time Travel",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+      ".",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
+      ".",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "Forecast Finesse: Analysing stock performance",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+      ".",
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
