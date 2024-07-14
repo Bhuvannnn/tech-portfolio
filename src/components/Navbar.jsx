dfe3951eb8a09c6e5334efdf6183be995e4c8a09
Bhuvan Shah <bhuvanshah288@gmail.com>
2024-07-13 17:17:46 -0700

    resume try

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithub, FaInstagram } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaInstagram, FaFilePdf } from 'react-icons/fa';
+import logo from '/Users/rahulshah/Desktop/tech-portfolio/src/assets/BS LOGO-JULY 2024.png';
 
-import logo from '/Users/rahulshah/Desktop/tech-portfolio/src/assets/kevinRushLogo.png';
 const Navbar = () => {
     const handleLinkedInClick = () => {
         window.open("https://www.linkedin.com/in/bhuvanshah/", "_blank");
@@ -14,18 +13,24 @@ const Navbar = () => {
     const handleInstagramClick = () => {
         window.open("https://www.instagram.com/bhu._.one/", "_blank");
     }
-    return(
-    <nav className="mb-20 flex items-center justify-between py-6">
 
-        <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo"/>
-        </div>
-        <div className=' flex m-8 items-center justify-center gap-4 text-2xl'> 
-            <FaLinkedin onClick={handleLinkedInClick} className='cursor-pointer'/>
-            <FaGithub onClick={handleGithubClick} className='cursor-pointer'/>
-            <FaInstagram onClick={handleInstagramClick} className='cursor-pointer'/>
-        </div>
-    </nav>
+    const handleResumeClick = () => {
+        const baseUrl = process.env.NODE_ENV === 'development' ? '' : '/tech-portfolio';
+        window.open(`${baseUrl}/Bhuvan%20Resume.pdf`, "_blank"); // Adjusted path
+    }
+
+    return (
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img className="mx-2 w-10" src={logo} alt="logo" />
+            </div>
+            <div className='flex m-8 items-center justify-center gap-4 text-2xl'>
+                <FaFilePdf onClick={handleResumeClick} className='cursor-pointer' />
+                <FaLinkedin onClick={handleLinkedInClick} className='cursor-pointer'/>
+                <FaGithub onClick={handleGithubClick} className='cursor-pointer'/>
+                <FaInstagram onClick={handleInstagramClick} className='cursor-pointer'/>
+            </div>
+        </nav>
     );
 };
 
